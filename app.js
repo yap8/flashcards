2d06d9609792cd6f93e6137e8f8572f6482d1fee
yap8 <nebozhenko.arseny@gmail.com>
2022-03-24 01:09:37 +0300

    renamed db

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 
 const PORT = process.env.PORT || 5000
-const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/mern-flashcards'
+const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/flashcards'
 
 const app = express()
 
