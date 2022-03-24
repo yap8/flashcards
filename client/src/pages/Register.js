a5038d671478854c0b077c1a402a2e02ae60efa1
yap8 <nebozhenko.arseny@gmail.com>
2022-03-24 19:50:00 +0300

    finished register form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Title from '../components/Title'
+import axios from 'axios'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -8,8 +9,26 @@ const Register = () => {
     password: ''
   })
 
-  const handleSubmit = e => {
-    e.preventDefault()
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async e => {
+    try {
+      e.preventDefault()
+  
+      const { name, email, password } = formData
+  
+      setLoading(true)
+
+      await axios.post('/api/users/register', {
+        name,
+        email,
+        password
+      })
+
+      setLoading(false)
+    } catch (error) {
+      alert(error.message)
+    }
   }
 
   return (
@@ -19,36 +38,42 @@ const Register = () => {
         <form className="form" onSubmit={handleSubmit}>
           <div className="form__group">
             <input
-              className="form__field"
+              className={"form__field" + (loading ? ' form__field--disabled' : '')}
               type="text"
               placeholder="Name"
               name="name"
               value={formData.name}
+              disabled={loading}
               onChange={e => setFormData({ ...formData, [e.target.name]: e.target.value })}
             />
           </div>
           <div className="form__group">
             <input
-              className="form__field"
+              className={"form__field" + (loading ? ' form__field--disabled' : '')}
               type="email"
               placeholder="Email"
               name="email"
               value={formData.email}
+              disabled={loading}
               onChange={e => setFormData({ ...formData, [e.target.name]: e.target.value })}
             />
           </div>
           <div className="form__group">
             <input
-              className="form__field"
+              className={"form__field" + (loading ? ' form__field--disabled' : '')}
               type="password"
               placeholder="Password"
               name="password"
               value={formData.password}
+              disabled={loading}
               onChange={e => setFormData({ ...formData, [e.target.name]: e.target.value })}
             />
           </div>
           <div className="form__group">
-            <button className="form__button">Register</button>
+            <button
+              className={'form__button' + (loading ? ' form__button--disabled' : '')}
+              disabled={loading}
+            >Register</button>
           </div>
         </form>
       </div>
