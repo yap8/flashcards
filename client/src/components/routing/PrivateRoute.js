c0ebf8509abb620f1d4d8b141c5732d00ea7d6d9
yap8 <nebozhenko.arseny@gmail.com>
2022-03-24 22:46:20 +0300

    removed PrivateRoute

--- client/src/components/routing/PrivateRoute.js
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom'
-
-const PrivateRoute = () =>
-  localStorage.getItem('authToken') ? <Outlet /> : <Navigate to="/login" />
-
-export default PrivateRoute
