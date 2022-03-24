7b415f7049bbf3ea71edd0bb64d739e09746cd23
yap8 <nebozhenko.arseny@gmail.com>
2022-03-24 22:39:40 +0300

    added conditional rendering for header nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,33 @@
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 
 const Header = () => {
+  const { authToken } = useSelector(state => state.user)
+
   return (
     <header className="header">
       <div className="header__inner container">
         <Link className="header__logo" to="/">FlashCards</Link>
         <ul className="header__nav">
-          <li className="header__nav-item">
-            <Link className="header__nav-link" to="/login">Login</Link>
-          </li>
-          <li className="header__nav-item">
-            <Link className="header__nav-link" to="/register">Register</Link>
-          </li>
-          <li className="header__nav-item">
+          {authToken ? <>
+            <li className="header__nav-item">
             <Link className="header__nav-link" to="/collections">Collections</Link>
-          </li>
-          <li className="header__nav-item">
-            <Link className="header__nav-link" to="/profile">Profile</Link>
-          </li>
-          <li className="header__nav-item">
-            <Link className="header__nav-link" to="/logout">Logout</Link>
-          </li>
+            </li>
+            <li className="header__nav-item">
+              <Link className="header__nav-link" to="/profile">Profile</Link>
+            </li>
+            <li className="header__nav-item">
+              <Link className="header__nav-link" to="/logout">Logout</Link>
+            </li>
+          </> : <>
+            <li className="header__nav-item">
+              <Link className="header__nav-link" to="/login">Login</Link>
+            </li>
+            <li className="header__nav-item">
+              <Link className="header__nav-link" to="/register">Register</Link>
+            </li>
+          </>}
         </ul>
       </div>
     </header>
