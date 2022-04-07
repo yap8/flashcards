b32f0db727d57c5457c1265108b0b808b3397fd0
yap8 <nebozhenko.arseny@gmail.com>
2022-04-07 23:00:35 +0300

    added Home buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import Title from '../components/Title'
 
 const Home = () => {
+  const { user } = useSelector(state => state.auth)
+
   return (
-    <section className="profile">
-      <div className="profile__inner container">
+    <section className="home">
+      <div className="home__inner container">
         <Title>Welcome</Title>
+        <ul className="home__buttons">
+          {user ? <>
+            <li className="home__buttons-item">
+              <Link className="home__link button button--small" to="/collections">
+                Collections
+              </Link>
+            </li>
+            <li className="home__buttons-item">
+              <Link className="home__link button button--small" to="/collections/create">
+                Create Collection
+              </Link>
+            </li>
+          </> : <>
+            <li className="home__buttons-item">
+              <Link className="home__link button" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="home__buttons-item">
+              <Link className="home__link button" to="/login">
+                Register
+              </Link>
+            </li>
+          </>}
+        </ul>
       </div>
     </section>
   )
