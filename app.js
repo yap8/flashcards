37ec43a2aea26e4ac43e2d29377b3cd1994f7327
yap8 <nebozhenko.arseny@gmail.com>
2022-04-07 21:02:10 +0300

    prepared for deployment

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 
+const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 
+const NODE_ENV = process.env.NODE_ENV || 'development'
 const PORT = process.env.PORT || 5000
 const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/flashcards'
 
@@ -16,6 +18,17 @@ app.use(cookieParser())
 // routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/collections', require('./routes/collectionRoutes'))
+
+// serve frontend
+if (NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './client/build')))
+
+  console.log(path.join(__dirname, './client/build'))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 // start
 const start = () => {
