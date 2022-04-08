9254c482c0c052638049d89ba6db370b23d2ef9d
yap8 <nebozhenko.arseny@gmail.com>
2022-04-08 20:57:46 +0300

    fixed classes for icon in Button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -33,7 +33,7 @@ const Button = ({ tag = 'button', dropdown, active, children, ...props }) => {
       { ...props }
     >
       { children }
-      <svg class="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+      <svg className="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
     </button>
   )
 
