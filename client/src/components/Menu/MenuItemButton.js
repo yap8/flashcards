222bbf33fecc767c51b2dfd94224f3f57e2d09fc
yap8 <nebozhenko.arseny@gmail.com>
2022-04-08 22:42:27 +0300

    fixed logout button styles

--- client/src/components/Menu/MenuItemButton.js
+++ client/src/components/Menu/MenuItemButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MenuItemButton = ({ children, ...props }) => {
-  const classes = 'block text-xl text-gray-500 hover:text-gray-900 py-2 px-4 transition bg-white hover:bg-gray-200 group-last:rounded-b group-first:rounded-t'
+  const classes = 'block w-full text-left text-xl text-gray-500 hover:text-gray-900 py-2 px-4 transition bg-white hover:bg-gray-200 group-last:rounded-b group-first:rounded-t'
 
   return (
     <li
