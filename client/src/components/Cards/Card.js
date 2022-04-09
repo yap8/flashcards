4219a9a438368943551ffbe1e864bf5e966c4974
yap8 <nebozhenko.arseny@gmail.com>
2022-04-09 20:52:36 +0300

    changed card styles

--- client/src/components/Cards/Card.js
+++ client/src/components/Cards/Card.js
@@ -1,8 +1,8 @@
 const Card = ({ card, index, handleClick }) => {
   return (
-    <li className="w-80 h-60 bg-white" style={{perspective: '1000px'}}>
+    <li className="w-80 h-60" style={{perspective: '1000px'}}>
       <button
-        className="w-full h-full relative transition rounded shadow-lg pointer"
+        className="w-full h-full bg-white relative shadow-lg rounded transition duration-200 pointer"
         style={{ transformStyle: 'preserve-3d', transform: card.flipped ? 'rotateY(180deg)' : '' }}
         onClick={() => handleClick(index)}
       >
@@ -14,7 +14,7 @@ const Card = ({ card, index, handleClick }) => {
         </div>
         <div
           className="w-full h-full flex text-center overflow-hidden absolute top-0"
-          style={{ backfaceVisibility: 'hidden', transform: 'rotateY(180deg)' }}
+          style={{backfaceVisibility: 'hidden', transform: 'rotateY(180deg)'}}
         >
           <h3 className="text-2xl m-auto">{card.back}</h3>
         </div>
