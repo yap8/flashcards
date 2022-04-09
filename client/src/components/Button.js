337a3f504c2dbc8bb93b5f44258a917961250c25
yap8 <nebozhenko.arseny@gmail.com>
2022-04-10 00:54:06 +0300

    moved spinner icon to icons folder

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import SpinnerIcon from './Icons/SpinnerIcon'
 
 const colors = [
   'white',
@@ -28,7 +29,7 @@ const Button = ({ tag, dropdown, className = '', children, ...props }) => {
   if (dropdown) return (
     <button className={classes} {...props}>
       {children}
-      <svg className="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+      {SpinnerIcon}
     </button>
   )
 
