8606f012f9961b0f294b555a1ec459246e60fd7b
yap8 <nebozhenko.arseny@gmail.com>
2022-05-01 15:12:28 +0300

    reworked dark theme logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,24 @@
-import { useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useSelector } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 import Header from './components/Header';
 import Pages from './pages/Pages';
 
 function App() {
-  const theme = useSelector(state => state.theme)
-
-  useEffect(() => {
-    if (theme === 'dark') {
-      document.body.classList.remove('bg-gray-100')
-      document.body.classList.add('bg-slate-700')
-      document.body.classList.add('dark')
-    } else {
-      document.body.classList.remove('dark')
-      document.body.classList.remove('bg-slate-700')
-      document.body.classList.add('bg-gray-100')
-    }
-  }, [theme])
+  const theme = useSelector((state) => state.theme);
 
   return (
-    <Router>
-      <ToastContainer position="bottom-center" />
-      <Header />
-      <Pages />
-    </Router>
+    <div className={theme}>
+      <div className="min-h-screen transition bg-gray-100 dark:bg-slate-700">
+        <Router>
+          <ToastContainer position="bottom-center" />
+          <Header />
+          <Pages />
+        </Router>
+      </div>
+    </div>
   );
 }
 
