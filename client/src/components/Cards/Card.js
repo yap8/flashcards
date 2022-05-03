cd811583fe4e3929377dbbfe4abeae614b9b1f91
yap8 <nebozhenko.arseny@gmail.com>
2022-05-03 19:40:26 +0300

    reworked card styles

--- client/src/components/Cards/Card.js
+++ client/src/components/Cards/Card.js
@@ -1,23 +1,26 @@
 import mergeClasses from '../../helpers/mergeClasses';
 
 const Card = ({ card, index, handleClick }) => {
-  const classes = {
-    base: 'w-full h-full bg-white relative shadow rounded transition duration-200 pointer preserve-3d',
-    hover: 'hover:bg-gray-50',
-    dark: 'dark:bg-slate-800 dark:text-white dark:hover:bg-slate-900',
+  const innerClasses = {
+    base: 'w-full h-full relative transition duration-200 pointer preserve-3d group',
     flipped: card.flipped ? 'rotate-y-180' : '',
+  };
+
+  const sideClasses = {
+    base: 'bg-white shadow rounded w-full h-full flex text-center overflow-hidden absolute top-0 backface-invisible transition group-hover:bg-gray-50',
+    dark: 'dark:bg-slate-800 dark:text-white dark:group-hover:bg-slate-900',
   };
 
   return (
     <li className="w-full h-60 mr-4 mb-4 md:w-80 perspective-1000">
       <button
-        className={mergeClasses(classes)}
+        className={mergeClasses(innerClasses)}
         onClick={() => handleClick(index)}
       >
-        <div className="w-full h-full flex text-center overflow-hidden absolute top-0 backface-invisible">
+        <div className={mergeClasses(sideClasses)}>
           <h3 className="text-2xl m-auto">{card.front}</h3>
         </div>
-        <div className="w-full h-full flex text-center overflow-hidden absolute top-0 backface-invisible rotate-y-180">
+        <div className={mergeClasses(sideClasses) + ' rotate-y-180'}>
           <h3 className="text-2xl m-auto">{card.back}</h3>
         </div>
       </button>
